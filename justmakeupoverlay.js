eac08f886c2acbe93b934859e511168b42341331
Alexsandria Manuel <alexsandriacreations@gmail.com>
2026-02-14 13:44:11 -0500

    Contact form and lightbox added

---
+++ justmakeupoverlay.js
@@ -0,0 +1,39 @@
+
+function openBox() {
+  document.getElementById("JMoverlay").style.display = "block";
+}
+
+function closeBox() {
+  document.getElementById("JMoverlay").style.display = "none";
+}
+
+
+
+var slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("slideshowimg");
+  var thumbnail = document.getElementsByClassName("slidethumbnail");
+  var overlaycaption = document.getElementById("lightboxoverlaycaption");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";
+  }
+  for (i = 0; i < thumbnail.length; i++) {
+      thumbnail[i].className = thumbnail[i].className.replace(" active", "");
+  }
+ slides[slideIndex-1].style.display = "block";
+  thumbnail[slideIndex-1].className += " active";
+  overlaycaption.innerHTML = thumbnail[slideIndex-1].alt;
+}
